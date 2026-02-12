1222331ab571828fb0acc36ba77685721a9588a3
masterjowo <daniajaOK@gmail.com>
2026-02-12 12:32:40 +0700

    12/02/2026 v1

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -13,16 +13,16 @@ public class BaseTest {
 
     public static WebDriver driver;
 
-    @BeforeMethod
-    public void setup() throws Exception {
-         driver = DriverFactory.createDriver();
-    }
+    // @BeforeMethod
+    // public void setup() throws Exception {
+    //      driver = DriverFactory.createDriver();
+    // }
 
-    @AfterMethod
-    public void teardown() {
-        System.out.println("AFTER METHOD JALAN");
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+    // @AfterMethod
+    // public void teardown() {
+    //     System.out.println("AFTER METHOD JALAN");
+    //     if (driver != null) {
+    //         driver.quit();
+    //     }
+    // }
 }
